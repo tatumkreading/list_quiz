a4ca6bd1e1ab10b72cc4f3e2703663875c1b6180
Tatum Reading <tatumreading@tatums-air.dcsdk12.org>
2019-10-08 09:25:16 -0600

    Final attempt quiz

---
+++ list_quiz.rb
@@ -0,0 +1,23 @@
+#three_even?
+
+def three_even?(arr)
+    arr.each do |x|
+        if nums/2 == 0
+            return true
+        else
+            return false
+    end
+end 
+
+puts three_even?([2, 1, 3, 5])
+puts three_even?([2, 5, 12, 5])
+
+bigger_two
+
+def bigger_two(x,y)
+    x>y ? a : b
+
+end
+
+bigger_two([1,2], [3,4])
+bigger_two([1, 7], [4,4])
